427471972c2496711d90090f70fa42ea3144164d
vladcemir <vladimir.kozak598@seznam.cz>
2024-08-14 08:27:34 +0200

    can be created multiple balls now

--- bnc_ball_w_controlls.py
+++ bnc_ball_w_controlls.py
@@ -1,4 +1,5 @@
 import turtle
+from random import randrange
 
 
 def exit():
@@ -36,48 +37,72 @@ def goRight():
     
 
 
-turtle.listen()
+
 width = 1000
 height = 700
 # Set window and ball
 window = turtle.Screen()
+tmp = window.numinput("How many balls?", "input your balls:", 1, minval=1, maxval=10000)
 window.setup(width, height)
 window.tracer(0)
 window.title("Bouncing Balls <<use arrows to controll>>")
 
-#ball init.. facing down, falling
-ball1 = turtle.Turtle()
-ball1.penup()
-ball1.setpos(-300,-300)
-ball1.shape("circle")
-#ball1.pendown()
-ball1.color("red")
-ball1.right(90)
 
-ball2 = turtle.Turtle()
-ball2.penup()
-ball2.setpos(-200, 200)
-ball2.shape("circle")
-#ball1.pendown()
-ball2.color("blue")
-ball2.right(90)
-
+colors = ["red", "blue", "green", "purple", "yellow", "pink"]
+ballcount = int(tmp)
 
 balls = []
 
-balls.append(ball1)
-balls.append(ball2)
+for x in range(ballcount):
+    ball = x
+    ball = turtle.Turtle()
+
+    ball.penup()
+    ball.setpos(randrange(-300, 300), randrange(-300, 300))
+    ball.shape("circle")
+    ball.color(colors[randrange(0,len(colors))])
+    ball.right(90)
+    balls.append(ball)
+
+
+
+# #ball init.. facing down, falling
+# ball1 = turtle.Turtle()
+# ball1.penup()
+# ball1.setpos(-300,-300)
+# ball1.shape("circle")
+# #ball1.pendown()
+# ball1.color("red")
+# ball1.right(90)
+
+# ball2 = turtle.Turtle()
+# ball2.penup()
+# ball2.setpos(-200, 200)
+# ball2.shape("circle")
+# #ball1.pendown()
+# ball2.color("blue")
+# ball2.right(90)
+
+
+
+
+# balls.append(ball1)
+# balls.append(ball2)
 
 
 energy_loss = 0.95
 newVelocity = 0
 gravity = -0.0009   # pixels/(time of iteration)^2
-velocityY = [1, 1]   
-velocityX = [0.25, 0.25]
+velocityY = []   
+velocityX = []
+for i in balls:
+    velocityY.append(1)
+    velocityX.append(0.25)
+
 ground = (-height/2) + 15
 wall = (width / 2) - 12
 seeling = height/2
-
+turtle.listen()
 turtle.onkeypress(throwUp, 'Up')
 turtle.onkeypress(goLeft, 'Left')
 turtle.onkeypress(goRight, 'Right')
